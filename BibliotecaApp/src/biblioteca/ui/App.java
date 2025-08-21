ab423cf1037f78d02b96d3c6eadda48b4c22ce36
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-08-21 14:41:43 +0200

    Classe App - ponto de entrada da aplicação (main

---
+++ BibliotecaApp/src/biblioteca/ui/App.java
@@ -0,0 +1,8 @@
+package biblioteca.ui;
+
+public class App {
+    public static void main(String[] args) {
+        ConsoleUI consoleUI = new ConsoleUI();
+        consoleUI.iniciarSistema();
+    }
+}
